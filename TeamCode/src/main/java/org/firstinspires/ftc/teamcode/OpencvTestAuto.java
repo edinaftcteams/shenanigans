09da4338b980694a9bc5ca636fa06beb28b7f045
coldinMN <36302664+coldinMN@users.noreply.github.com>
2018-10-21 17:05:52 -0500

    Add files via upload
    Initial Drive Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpencvTestAuto.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="OpencvTestAuto", group="Pushbot")
+
+public class OpencvTestAuto extends LinearOpMode {
+
+
+
+
+    public void runOpMode(){
+
+
+
+    }
+
+}
